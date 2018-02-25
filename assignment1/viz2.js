eaef9f0c450f1e416d0de6a1e39a8c90a8823d31
Roamix <s144224@student.dtu.dk>
2018-02-25 23:44:53 +0100

    viz2 done!

--- assignment1/viz2.js
+++ assignment1/viz2.js
@@ -65,14 +65,10 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
         xaxis = d3.axisBottom(xscale);
         yaxis = d3.axisLeft(yscale);
 
-        var svg = d3.select("#viz2")
+        var svg = d3.select(".viz2")
                     .append('svg')
                     .attr('width', w)
                     .attr('height', h)
-
-        // var div = d3.select("body").append("div")
-        //         .attr("class", "tooltip")
-        //         .style("opacity", 0);
 
         ////// SCATTER PLOT ///////
         var formatTime = d3.timeFormat("%Y");
@@ -106,7 +102,11 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
             svg.selectAll(".legend_text")
                 .remove();
 
+            svg.selectAll(".trendline")
+                .remove();
+
             if (toPlot == "Both") {
+                /////////// MEN ///////////
                 svg.append("g")
                     .attr('id', 'men')
                     .selectAll("circle")
@@ -148,7 +148,7 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
                     })
                     .on('mouseout', function(d) {
                        d3.select(this)
-                          .transition("restoreBarColor")
+                          .transition()
                           .duration(250)
                           .attr('fill', colors["Men"]);
 
@@ -158,6 +158,52 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
                     .duration(1000)
                     .attr('opacity', 1);
 
+                    // ///TRENDLINE///
+                    // MEN
+                    var mxLabels = data_men.map(function (d) { return d["Year"]; })
+            		var mxSeries = d3.range(1, mxLabels.length + 1);
+            		var mySeries = data_men.map(function(d) { return parseFloat(d['Time']); });
+                    // WOMEN
+                    var wxLabels = data_women.map(function (d) { return d["Year"]; })
+            		var wxSeries = d3.range(1, wxLabels.length + 1);
+            		var wySeries = data_women.map(function(d) { return parseFloat(d['Time']); });
+
+            		var men_leastSquaresCoeff = leastSquares(mxSeries, mySeries);
+                    var women_leastSquaresCoeff = leastSquares(wxSeries, wySeries);
+
+            		// MEN
+            		var mx1 = mxLabels[0];
+            		var my1 = men_leastSquaresCoeff[0] + men_leastSquaresCoeff[1];
+            		var mx2 = mxLabels[mxLabels.length - 1];
+            		var my2 = men_leastSquaresCoeff[0] * mxSeries.length + men_leastSquaresCoeff[1];
+            		// var mtrendData = [[mx1,my1,mx2,my2]];
+
+                    // WOMEN
+            		var wx1 = wxLabels[0];
+            		var wy1 = women_leastSquaresCoeff[0] + women_leastSquaresCoeff[1];
+            		var wx2 = wxLabels[wxLabels.length - 1];
+            		var wy2 = women_leastSquaresCoeff[0] * wxSeries.length + women_leastSquaresCoeff[1];
+
+                    var trendData = [[mx1,my1,mx2,my2],[wx1,wy1,wx2,wy2]];
+
+            		var trendline = svg.selectAll(".trendline")
+            			.data(trendData);
+
+            		trendline.enter()
+            			.append("line")
+                        .attr('opacity', 0)
+            			.attr("class", "trendline")
+            			.attr("x1", function(d) { return xscale(d[0]); })
+            			.attr("y1", function(d) { return yscale(d[1]); })
+            			.attr("x2", function(d) { return xscale(d[2]); })
+            			.attr("y2", function(d) { return yscale(d[3]); })
+            			.attr("stroke", "black")
+            			.attr("stroke-width", 1)
+                        .transition()
+                        .duration(1000)
+                        .attr('opacity', 1);
+
+                ////////// WOMEN //////////
                 svg.append("g")
                     .attr('id', 'women')
                     .selectAll("circle")
@@ -199,7 +245,7 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
                     })
                     .on('mouseout', function(d) {
                        d3.select(this)
-                          .transition("restoreBarColor")
+                          .transition()
                           .duration(250)
                           .attr('fill', colors["Women"]);
 
@@ -209,41 +255,53 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
                     .duration(1000)
                     .attr('opacity', 1);
 
+                //// LEGEND /////
                 svg.append("text")
+                    .attr('opacity', 0)
                     .attr('class', 'legend_text')
                     .attr("transform",
                     "translate(" + (w - padding*2 - 5) + "," + (h - 300) + ")")
                     .style("text-anchor", "left")
-                    .text("Men");
+                    .text("Men")
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
 
                 svg.append("text")
+                    .attr('opacity', 0)
                     .attr('class', 'legend_text')
                     .attr("transform",
                     "translate(" + (w - padding*2 - 5) + "," + (h - 280) + ")")
                     .style("text-anchor", "left")
-                    .text("Women");
+                    .text("Women")
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
 
                 svg.append("circle")
+                    .attr('opacity', 0)
                     .attr('class', 'legend_circle')
                     .attr('cx', w - padding*2 - 15)
                     .attr('cy', h - 305)
                     .attr("r", 3)
-                    .attr('fill', colors["Men"]);
+                    .attr('fill', colors["Men"])
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
 
                 svg.append("circle")
+                    .attr('opacity', 0)
                     .attr('class', 'legend_circle')
                     .attr('cx', w - padding*2 - 15)
                     .attr('cy', h - 285)
                     .attr("r", 3)
-                    .attr('fill', colors["Women"]);
+                    .attr('fill', colors["Women"])
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
 
             } else {
-                ///TRENDLINE///
-                // MEN INTERCEPT: -0.29038071
-                // MEN COEFF: 709.713060561
 
-                // WOMEN INTERCEPT: -0.93566821
-                // WOMEN COEFF: 2018.72386009
 
                 svg.selectAll("circle")
                     .data(data)
@@ -285,7 +343,7 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
                     })
                     .on('mouseout', function(d) {
                        d3.select(this)
-                          .transition("restoreBarColor")
+                          .transition()
                           .duration(250)
                           .attr('fill', colors[toPlot]);
 
@@ -297,20 +355,58 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
 
                 //// LEGEND ////
                 svg.append("text")
+                    .attr('opacity', 0)
                     .attr('class', 'legend_text')
                     .attr("transform",
                     "translate(" + (w - padding*2 - 5) + "," + (h - 300) + ")")
                     .style("text-anchor", "left")
-                    .text(toPlot);
+                    .text(toPlot)
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
 
                 svg.append("circle")
+                    .attr('opacity', 0)
                     .attr('class', 'legend_circle')
                     .attr('cx', w - padding*2 - 15)
                     .attr('cy', h - 305)
                     .attr("r", 3)
-                    .attr('fill', colors[toPlot]);
+                    .attr('fill', colors[toPlot])
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
 
-            }
+                // ///TRENDLINE///
+                var xLabels = data.map(function (d) { return d["Year"]; })
+        		var xSeries = d3.range(1, xLabels.length + 1);
+        		var ySeries = data.map(function(d) { return parseFloat(d['Time']); });
+
+        		var leastSquaresCoeff = leastSquares(xSeries, ySeries);
+
+        		// apply the reults of the least squares regression
+        		var x1 = xLabels[0];
+        		var y1 = leastSquaresCoeff[0] + leastSquaresCoeff[1];
+        		var x2 = xLabels[xLabels.length - 1];
+        		var y2 = leastSquaresCoeff[0] * xSeries.length + leastSquaresCoeff[1];
+        		var trendData = [[x1,y1,x2,y2]];
+
+        		var trendline = svg.selectAll(".trendline")
+        			.data(trendData);
+
+        		trendline.enter()
+        			.append("line")
+                    .attr('opacity', 0)
+        			.attr("class", "trendline")
+        			.attr("x1", function(d) { return xscale(d[0]); })
+        			.attr("y1", function(d) { return yscale(d[1]); })
+        			.attr("x2", function(d) { return xscale(d[2]); })
+        			.attr("y2", function(d) { return yscale(d[3]); })
+        			.attr("stroke", "black")
+        			.attr("stroke-width", 1)
+                    .transition()
+                    .duration(1000)
+                    .attr('opacity', 1);
+            };
 
             svg.select(".x.axis")
                 .transition()
@@ -354,3 +450,25 @@ d3.csv('data\\part4_mens_open.csv', rowConverter, function(data_men) {
 
     });
 });
+
+function leastSquares(xSeries, ySeries) {
+    var reduceSumFunc = function(prev, cur) { return prev + cur; };
+
+    var xBar = xSeries.reduce(reduceSumFunc) * 1.0 / xSeries.length;
+    var yBar = ySeries.reduce(reduceSumFunc) * 1.0 / ySeries.length;
+
+    var ssXX = xSeries.map(function(d) { return Math.pow(d - xBar, 2); })
+        .reduce(reduceSumFunc);
+
+    var ssYY = ySeries.map(function(d) { return Math.pow(d - yBar, 2); })
+        .reduce(reduceSumFunc);
+
+    var ssXY = xSeries.map(function(d, i) { return (d - xBar) * (ySeries[i] - yBar); })
+        .reduce(reduceSumFunc);
+
+    var slope = ssXY / ssXX;
+    var intercept = yBar - (xBar * slope);
+    var rSquare = Math.pow(ssXY, 2) / (ssXX * ssYY);
+
+    return [slope, intercept, rSquare];
+}
